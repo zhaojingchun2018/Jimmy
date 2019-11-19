fefe2a83f7ea6ba4eb84a85f50681bed59fc70e6
zhaojingchun2018@sina.com <zhaojingchun2018@sina.com>
2019-11-19 17:10:24 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jimmy1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
