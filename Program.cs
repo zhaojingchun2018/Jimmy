d46329d6d8fea4f51753127cbde88036a8b3bd15
zhaojingchun2018@sina.com <zhaojingchun2018@sina.com>
2019-11-22 14:20:02 +0800

    修改第一章作业。

--- Program.cs
+++ Program.cs
@@ -6,9 +6,14 @@ namespace Jimmy1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            const string name = "Jimmy";
-            Console.WriteLine(name+"源栈欢迎你");
+            //第一章作业
+            
+            //1.安装Visual Studio 2017社区版
+            //2.新建一个控制台项目
+            //3.用变量name存储你的姓名，然后输出带有你姓名的入栈口号
+            
+            //const string name = "Jimmy";
+            //Console.WriteLine(name+"源栈欢迎你");
 
                 
         }
